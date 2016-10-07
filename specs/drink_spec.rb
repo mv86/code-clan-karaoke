dd336e3c0c2bcb3d174ef11fb20dee4c8903e114
Max <mveasey87@gmail.com>
2016-10-07 14:48:07 +0100

    drink class added

---
+++ specs/drink_spec.rb
@@ -0,0 +1,28 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../drink')
+
+class TestPerson < MiniTest::Test
+
+  def setup
+    @drink_1 = Drink.new('vodka & coke', 2.50)
+    @drink_2 = Drink.new('beer', 4.00)
+    @drink_3 = Drink.new('coke', 1.00)
+    @drink_4 = Drink.new('water', 0)
+  end
+
+  def test_drink_has_name
+    assert_equal('beer', @drink_2.name)
+  end
+
+  def test_drink_has_price
+    assert_equal(1.00, @drink_3.price)
+  end
+
+
+
+
+
+
+
+end
